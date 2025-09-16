ad490a4905a48deacfa62ccaa62f56c5a0135182
Thomas Griffin <40271229+Thomas-Griffin@users.noreply.github.com>
2025-09-16 16:31:56 +0100

    Use a variable for vehicle movement speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float vehicleSpeed = 20;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * 20));
+        transform.Translate(Vector3.forward * (Time.deltaTime * vehicleSpeed));
     }
 }
