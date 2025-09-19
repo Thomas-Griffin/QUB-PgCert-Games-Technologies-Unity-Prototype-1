08e8285c5dfc1b94c4d9c92ae226cbcca966b171
Thomas Griffin <40271229+Thomas-Griffin@users.noreply.github.com>
2025-09-19 08:23:42 +0100

    Add basic vehicle turn functionality to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
     public float vehicleSpeed = 20;
+    public float vehicleTurnSpeed = 10;
+    public float horizontalInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,7 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // We get the horizontal input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * (Time.deltaTime * vehicleSpeed));
+        // And turn the vehicle based on the turn speed and horizontal input
+        transform.Translate(Vector3.right * (Time.deltaTime * vehicleTurnSpeed * horizontalInput));
     }
 }
